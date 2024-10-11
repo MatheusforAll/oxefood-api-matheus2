d3eab15e22b3a23c2af053c018f6acd9e92a9db8
aluno <ifpe>
2024-10-10 21:25:22 -0300

    primeiros arquivos do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiMatheus2Application.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiMatheus2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiMatheus2Application.class, args);
+	}
+
+}
