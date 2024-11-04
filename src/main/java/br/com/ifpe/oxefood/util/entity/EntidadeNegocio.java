74b556a4e1b58cc5e3c1f1ea1f2097438dfbdb2c
aluno <ifpe>
2024-11-04 19:46:03 -0300

    conteudo 9

--- src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeNegocio.java
+++ src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeNegocio.java
@@ -1,8 +1,15 @@
 package br.com.ifpe.oxefood.util.entity;
 
 
-    import java.io.Serializable;
+import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,11 +17,17 @@ import lombok.Setter;
 @Getter
 @Setter
 @EqualsAndHashCode(of = { "id" })
+@MappedSuperclass
 public abstract class EntidadeNegocio implements Serializable {
 
-    private Long id;
+   @Id
+   @GeneratedValue(strategy = GenerationType.SEQUENCE)
+   private Long id;
 
-    private Boolean habilitado;
+   @JsonIgnore
+   @Column
+   private Boolean habilitado;
+
     
 }
 
